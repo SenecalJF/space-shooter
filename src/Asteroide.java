a563fdab2746fdcb14e00cc8efd82043f77ff734
Alex_Des <alexis.desforges@icloud.com>
2020-12-01 21:21:49 -0500

    Réparation des déplacements d'astéroides et du vaisseau Spawn d'astéroides ajouté

--- src/Asteroide.java
+++ src/Asteroide.java
@@ -1,49 +1,63 @@
 import org.newdawn.slick.Image;
 
+import java.util.Random;
 import java.util.ArrayList;
 
 public class Asteroide extends Entite {
 
+    int directionAsteroide;
+    double vitesseDescente;
+    double deplacementY;
+    Random random = new Random();
+
+
     public Asteroide(int x, int y, int width, int height, Image imagepath) {
         super(x, y, width, height, imagepath);
-
-
+        int possVit = random.nextInt(4);
+        switch (possVit) {
+            case 0:
+                this.vitesseDescente = 0.045;
+                break;
+            case 1:
+                this.vitesseDescente = 0.050;        // 4 vitesses possibles
+                break;
+            case 2:
+                this.vitesseDescente = 0.025;
+                break;
+            case 3:
+                this.vitesseDescente = 0.010;
+                break;
+        }
+        this.directionAsteroide = random.nextInt(2);
     }
 
     @Override
-    public void update(int delta, int direction) {
-        if (x > MainClass.LARGEUR - 120) {
-            direction = 1;
+    public void update(int delta) {
+
+        this.deplacementY += this.vitesseDescente * delta;
+        if (deplacementY >= 1) {                                     //ca permet des vitesses plus petites que 1 pixel/update
+            this.y++;
+            this.deplacementY = 0;
         }
 
 
-        if (x < 50) {
-            direction = 3;
+        if (this.directionAsteroide == 0) {
+            if (this.x >= MainClass.LARGEUR - this.width) {
+                this.directionAsteroide = 1;
+            } else {
+                this.x += Math.round(delta * 0.08);
+            }
+
+        } else if (this.directionAsteroide == 1) {
+            if (this.x <= 1) {
+                this.directionAsteroide = 0;
+            } else {
+                this.x -= Math.round(delta * 0.08);
+            }
         }
-
-
-        if (y > MainClass.HAUTEUR - 100) {
-            direction = 0;
-        }
-
-
-        if (y < 50) {
-            direction = 2;
-        }
-
-
-        if (direction == 2) {
-            y -= 0.1 * delta;
-        } else if (direction == 0) {
-            y += 0.1 * delta;
-        }
-
-        if (direction == 1) {
-            x -= 0.1 * delta;
-        } else if (direction == 3) {
-            x += 0.1 * delta;
-        }
-
 
     }
+
+
+
 }
